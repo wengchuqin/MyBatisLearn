2dfe04dc5f7b8ee73843f35d73cebe2da036dd75
wengchuqin <33614506+wengchuqin@users.noreply.github.com>
2017-12-25 10:58:45 +0800

    Update UserMapper.java

--- src/main/java/top/chuqin/learn/mybatis/mapper/UserMapper.java
+++ src/main/java/top/chuqin/learn/mybatis/mapper/UserMapper.java
@@ -1,8 +1,9 @@
 package top.chuqin.learn.mybatis.mapper;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.*;
 import top.chuqin.learn.mybatis.domain.User;
+
+import java.util.List;
 
 /**
  * Created by wengchuqin on 2017/12/25.
@@ -13,4 +14,21 @@ public interface UserMapper {
     @Options(useGeneratedKeys = true, keyProperty = "id")
     int saveUser(User user);
 
+    @Delete("DELETE FROM tb_user WHERE id = #{id}")
+    int removeUser(@Param("id") Integer id);
+
+    @Update("UPDATE tb_user SET name = #{name}, sex = #{sex}, age = #{age} WHERE id = #{id}")
+    void modifyUser(User user);
+
+    @Select("SELECT * FROM tb_user WHERE id = #{id}")
+    @Results({
+            @Result(id = true, column = "id", property = "id"),
+            @Result(column = "name", property = "name"),
+            @Result(column = "sex", property = "sex"),
+            @Result(column = "age", property = "age")
+    })
+    User selectUserById(Integer id);
+
+    @Select("SELECT * FROM tb_user")
+    List<User> selectAllUser();
 }
